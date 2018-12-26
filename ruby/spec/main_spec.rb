660cdae5bf5a4187ba9acf53a8a69415309755ea
Sean Marzug-McCarthy <samdec11@gmail.com>
2018-12-26 14:30:01 -0500

    Allow multiple chars, show when lacking openers or closers

--- ruby/spec/main_spec.rb
+++ ruby/spec/main_spec.rb
@@ -1,25 +1,62 @@
 require_relative "../src/main.rb"
 
 RSpec.describe "Main" do
-  describe "run" do
-    subject { Main.run(input) }
+  describe "assess" do
+    subject { Main.assess(input) }
 
-    context "when parentheses are evenly balanced" do
-      let(:input) { "(text)" }
+    context "when curly braces are evenly balanced" do
+      let(:input) { "{text}" }
 
       it { is_expected.to eq("This is valid") }
     end
 
-    context "when parentheses are not evenly balanced" do
-      let(:input) { "(text" }
+    context "when curly braces are not evenly balanced" do
+      let(:input) { "{text" }
 
-      it do
-        is_expected.to eq(
+      it { is_expected.to eq("Unclosed { at index 0") }
+    end
+
+    context "with multiple openers and closers" do
+      context "when balanced" do
+        let(:input) do
           <<~EOS
-            (text
-            ^    
+            {
+              "list": [{
+                "text": "This (is some) text"
+              }]
+            }
           EOS
-        )
+        end
+
+        it { is_expected.to eq("This is valid") }
+      end
+
+      context "when an opener is missing a closer" do
+        let(:input) do
+          <<~EOS
+            {
+              "list": [{
+                "text": "This (is some) text"
+              ]
+            }
+          EOS
+        end
+
+        it { is_expected.to eq("Unclosed { at index 13") }
+      end
+
+      context "when a closer is missing an opener" do
+        let(:input) do
+          <<~EOS
+            {
+              "list": [
+                "text": "This (is some) text"
+              }]
+            }
+          EOS
+        end
+
+        it { is_expected.to eq("Unopened } at index 50")}
       end
     end
   end
--- ruby/src/main.rb
+++ ruby/src/main.rb
@@ -1,33 +1,54 @@
 class Main
   CHARSET = {
+    "{" => "}",
     "(" => ")",
+    "[" => "]",
   }.freeze
 
-  def self.run(input)
-    length = input.length
-    openers = []
-    closers = []
-    chars = input.split("")
-    chars.each_with_index do |char, index|
-      if CHARSET.keys.include?(char)
-        openers << [char, index]
-      elsif CHARSET.values.include?(char)
-        closers << [char, index]
+  class << self
+    def assess(input)
+      length = input.length
+      openers = []
+      closers = []
+      chars = input.split("")
+      chars.each_with_index do |char, index|
+        if CHARSET.keys.include?(char)
+          openers << [char, index]
+        elsif CHARSET.values.include?(char)
+          closers.unshift([char, index])
+        end
       end
+
+      error = nil
+
+      if openers.length > closers.length
+        error = find_mismatch(openers, closers)
+        message = "Unclosed #{error[:character]} at index #{error[:index]}"
+      elsif closers.length > openers.length
+        error = find_mismatch(closers, openers)
+        message = "Unopened #{error[:character]} at index #{error[:index]}"
+      else
+        message = "This is valid"
+      end
+      message
     end
 
-    if openers.length == closers.length
-      return "This is valid"
+    def run(input)
+      puts assess(input)
     end
-    output = "#{input}\n"
-    pointer = " " * length + "\n"
-    openers.each_with_index do |(char, i), index|
-      if closers.fetch(index, [])[0] != CHARSET[char]
-        pointer[i] = "^"
+
+    private
+
+    def find_mismatch(first_array, second_array)
+      error = nil
+      first_array.each_with_index do |(char, i), index|
+        expected = CHARSET.keys.include?(char) ? CHARSET[char] : CHARSET.invert[char]
+        if second_array.fetch(index, [])[0] != expected
+          error = {character: char, index: i}
+          break
+        end
       end
+      error
     end
-    output += pointer
-    puts output
-    output
   end
 end
